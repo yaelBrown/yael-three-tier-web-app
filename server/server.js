5c4ba3f2181ac3738c5f8efd48c962050cc133bc
yaelBrown <yaelrbrown@gmail.com>
2025-12-26 17:25:30 -0500

    feat: can pull data and add data to backend.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 8080;
 
 const db_messages = require('./messages.json');
 
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+}))
 
 app.get('/', (req, res) => {
   res.send('Hello World from Express!');
